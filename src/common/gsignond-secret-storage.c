31b8854d7c378beb2ec9d0daa441ff99c535609d
Imran Zaman <imran.zaman@linux.intel.com>
2012-12-19 10:56:25 +0200

    daemon: added interface object for secret storage

---
+++ src/common/gsignond-secret-storage.c
@@ -0,0 +1,222 @@
+/* vi: set et sw=4 ts=4 cino=t0,(0: */
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of gsignond
+ *
+ * Copyright (C) 2012 Intel Corporation.
+ *
+ * Contact: Imran Zaman <imran.zaman@linux.intel.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+#include "gsignond-secret-storage.h"
+
+#define GSIGNOND_SECRET_STORAGE_GET_PRIVATE(obj) \
+                                          (G_TYPE_INSTANCE_GET_PRIVATE ((obj),\
+                                           GSIGNOND_TYPE_SECRET_STORAGE, \
+                                           GSignondSecretStoragePrivate))
+
+struct _GSignondSecretStoragePrivate
+{
+};
+
+G_DEFINE_TYPE (GSignondSecretStorage, gsignond_secret_storage,
+        G_TYPE_OBJECT);
+
+gboolean
+gsignond_secret_storage_open_db (
+        GSignondSecretStorage *self,
+        GSignondConfig* configuration)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->open_db (self,
+            configuration);
+}
+
+gboolean
+gsignond_secret_storage_close_db (
+        GSignondSecretStorage *self)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->close_db (self);
+}
+
+gboolean
+gsignond_secret_storage_clear_db (
+        GSignondSecretStorage *self)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->clear_db (self);
+}
+
+gboolean
+gsignond_secret_storage_is_open_db (
+        GSignondSecretStorage *self)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->is_open_db (self);
+}
+
+gboolean
+gsignond_secret_storage_load_credentials (
+        GSignondSecretStorage *self,
+        const guint32 id,
+        GString *username,
+        GString *password)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->load_credentials (self,
+            id, username, password);
+}
+
+gboolean
+gsignond_secret_storage_update_credentials (
+        GSignondSecretStorage *self,
+        const guint32 id,
+        const GString *username,
+        const GString *password)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->update_credentials (
+            self, id, username, password);
+}
+
+gboolean
+gsignond_secret_storage_remove_credentials (
+        GSignondSecretStorage *self,
+        const guint32 id)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->remove_credentials (
+            self, id);
+}
+
+gboolean
+gsignond_secret_storage_check_credentials (
+        GSignondSecretStorage *self,
+        const guint32 id,
+        GString *username,
+        GString *password)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->check_credentials (self,
+            id, username, password);
+}
+
+GHashTable*
+gsignond_secret_storage_load_data (
+        GSignondSecretStorage *self,
+        const guint32 id,
+        const guint32 method)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), NULL);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->load_data (self,
+            id, method);
+}
+
+gboolean
+gsignond_secret_storage_update_data (
+        GSignondSecretStorage *self,
+        const guint32 id,
+        const guint32 method,
+        const GHashTable *data)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->update_data (self,
+            id, method, data);
+}
+
+gboolean
+gsignond_secret_storage_remove_data (
+        GSignondSecretStorage *self,
+        const guint32 id,
+        const guint32 method)
+{
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+
+    return GSIGNOND_SECRET_STORAGE_GET_CLASS (self)->remove_data (self,
+            id, method);
+}
+
+static gboolean
+_gsignond_secret_storage_check_credentials (
+        GSignondSecretStorage *self,
+        const guint32 id,
+        GString *username,
+        GString *password)
+{
+    GString *stored_username = NULL;
+    GString *stored_password = NULL;
+    gboolean comp_uname = FALSE;
+    gboolean comp_pwd = FALSE;
+
+    GSignondSecretStorageClass *klass =
+            GSIGNOND_SECRET_STORAGE_GET_CLASS (self);
+
+    g_return_val_if_fail (GSIGNOND_IS_SECRET_STORAGE (self), FALSE);
+    klass->load_credentials (self, id,
+            stored_username, stored_username);
+    if (stored_username != NULL) {
+        comp_uname = g_string_equal(stored_username, username);
+        g_string_free(stored_username, TRUE);
+    }
+    if (stored_password != NULL) {
+        comp_pwd = g_string_equal(stored_password, password);
+        g_string_free(stored_password, TRUE);
+    }
+    return comp_uname && comp_pwd;
+}
+
+static void
+gsignond_secret_storage_class_init (
+        GSignondSecretStorageClass *klass)
+{
+    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+    /* pure virtual methods */
+    klass->open_db = NULL;
+    klass->close_db = NULL;
+    klass->clear_db = NULL;
+    klass->is_open_db = NULL;
+    klass->load_credentials = NULL;
+    klass->update_credentials = NULL;
+    klass->remove_credentials = NULL;
+    klass->load_data = NULL;
+    klass->update_data = NULL;
+    klass->remove_data = NULL;
+
+    /* virtual methods */
+    klass->check_credentials = _gsignond_secret_storage_check_credentials;
+}
+
+static void
+gsignond_secret_storage_init (
+        GSignondSecretStorage *self)
+{
+}
+
+
---
+++ src/common/gsignond-secret-storage.h
@@ -0,0 +1,270 @@
+/* vi: set et sw=4 ts=4 cino=t0,(0: */
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of gsignond
+ *
+ * Copyright (C) 2012 Intel Corporation.
+ *
+ * Contact: Imran Zaman <imran.zaman@linux.intel.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+#ifndef __GSIGNOND_SECRET_STORAGE_H__
+#define __GSIGNOND_SECRET_STORAGE_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include "gsignond-config.h"
+
+G_BEGIN_DECLS
+
+/*
+ * Type macros.
+ */
+#define GSIGNOND_TYPE_SECRET_STORAGE   \
+                                       (gsignond_secret_storage_get_type ())
+#define GSIGNOND_SECRET_STORAGE(obj)   (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+                                           GSIGNOND_TYPE_SECRET_STORAGE, \
+                                           GSignondSecretStorage))
+#define GSIGNOND_IS_SECRET_STORAGE(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+                                           GSIGNOND_TYPE_SECRET_STORAGE))
+#define GSIGNOND_SECRET_STORAGE_CLASS(klass) \
+                                            (G_TYPE_CHECK_CLASS_CAST ((klass), \
+                                             GSIGNOND_TYPE_SECRET_STORAGE, \
+                                             GSignondSecretStorageClass))
+#define GSIGNOND_IS_SECRET_STORAGE_CLASS(klass) \
+                                            (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+                                             GSIGNOND_TYPE_SECRET_STORAGE))
+#define GSIGNOND_SECRET_STORAGE_GET_CLASS(obj) \
+                                            (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+                                             GSIGNOND_TYPE_SECRET_STORAGE, \
+                                             GSignondSecretStorageClass))
+
+typedef struct _GSignondSecretStoragePrivate GSignondSecretStoragePrivate;
+
+typedef struct
+{
+    GObject parent_instance;
+
+    /*< private >*/
+    GSignondSecretStoragePrivate *priv;
+} GSignondSecretStorage;
+
+typedef struct {
+    GObjectClass parent_class;
+
+    /**
+     * open_db:
+     * @self: instance of #GSignondSecretStorage
+     *
+     * @configuration: A dictionary of configuration options, dependent on
+     * the specific storage implementation.
+     *
+     * Opens (and initializes) DB. The implementation should take
+     * care of creating the DB, if it doesn't exist.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*open_db) (GSignondSecretStorage *self,
+                         GSignondConfig* configuration);
+
+    /**
+     * close_db:
+     * @self: instance of #GSignondSecretStorage
+     *
+     * Closes the secrets DB. To reopen it, call open_db().
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*close_db) (GSignondSecretStorage *self);
+
+    /**
+     * clear_db:
+     * @self: instance of #GSignondSecretStorage
+     *
+     * Removes all stored secrets.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*clear_db) (GSignondSecretStorage *self);
+
+    /**
+     * is_open_db:
+     * @self: instance of #GSignondSecretStorage
+     *
+     * Checks if the db is open or not.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*is_open_db) (GSignondSecretStorage *self);
+
+    /**
+     * load_credentials:
+     * @self: instance of #GSignondSecretStorage
+     * @id: the identity whose credentials are being loaded.
+     * @username: the username.
+     * @password: the password.
+     *
+     * Loads the credentials.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*load_credentials) (GSignondSecretStorage *self,
+                                  const guint32 id,
+                                  GString *username,
+                                  GString *password);
+
+    /**
+     * update_credentials:
+     * @self: instance of #GSignondSecretStorage
+     * @id: the identity whose credentials are being updated.
+     * @username: the username.
+     * @password: the password.
+     *
+     * Stores/updates the credentials for the given identity.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*update_credentials) (GSignondSecretStorage *self,
+                                    const guint32 id,
+                                    const GString *username,
+                                    const GString *password);
+
+    /**
+     * remove_credentials:
+     * @self: instance of #GSignondSecretStorage
+     * @id: the identity whose credentials are being updated.
+     *
+     * Remove the credentials for the given identity.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*remove_credentials) (GSignondSecretStorage *self,
+                                    const guint32 id);
+
+    /**
+     * check_credentials:
+     * @self: instance of #GSignondSecretStorage
+     * @id: the identity whose credentials are being checked.
+     * @username: the username.
+     * @password: the password.
+     *
+     * Checks whether the given username and passwords are correct for the
+     * given identity.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*check_credentials) (GSignondSecretStorage *self,
+                                   const guint32 id,
+                                   GString *username,
+                                   GString *password);
+
+    /**
+     * load_data:
+     * @self: instance of #GSignondSecretStorage
+     * @id: the identity whose credentials are being checked.
+     * @method: the authentication method the data is used for.
+     * @data: #GHashTable dictionary with the data.
+     *
+     * Loads secret data.
+     *
+     * Returns: (transfer full) #GHashTable dictionary with the data.
+     */
+    GHashTable* (*load_data) (GSignondSecretStorage *self,
+                              const guint32 id,
+                              const guint32 method);
+
+    /**
+     * update_data:
+     * @self: instance of #GSignondSecretStorage
+     * @id: the identity whose credentials are being checked.
+     * @method: the authentication method the data is used for.
+     * @data: #GHashTable dictionary with the data.
+     *
+     * Stores/replaces secret data. Calling this method replaces any data
+     * which was previously stored for the given id/method.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*update_data) (GSignondSecretStorage *self,
+                             const guint32 id,
+                             const guint32 method,
+                             const GHashTable *data);
+    /**
+     * remove_data:
+     * @self: instance of #GSignondSecretStorage
+     * @id: the identity whose credentials are being checked.
+     * @method: the authentication method the data is used for.
+     *
+     * Removes secret data.
+     *
+     * Returns: TRUE if successful, FALSE otherwise.
+     */
+    gboolean (*remove_data) (GSignondSecretStorage *self,
+                             const guint32 id,
+                             const guint32 method);
+
+} GSignondSecretStorageClass;
+
+/* used by GSIGNOND_TYPE_SECRET_STORAGE */
+GType       gsignond_secret_storage_get_type (void);
+
+
+gboolean    gsignond_secret_storage_open_db (
+                GSignondSecretStorage *self,
+                GSignondConfig* configuration);
+
+gboolean    gsignond_secret_storage_close_db (
+                GSignondSecretStorage *self);
+
+gboolean    gsignond_secret_storage_clear_db (
+                GSignondSecretStorage *self);
+
+gboolean    gsignond_secret_storage_is_open_db (
+                GSignondSecretStorage *self);
+
+gboolean    gsignond_secret_storage_load_credentials (
+                GSignondSecretStorage *self, const guint32 id,
+                GString *username, GString *password);
+
+gboolean    gsignond_secret_storage_update_credentials (
+                GSignondSecretStorage *self, const guint32 id,
+                const GString *username, const GString *password);
+
+gboolean    gsignond_secret_storage_remove_credentials (
+                GSignondSecretStorage *self, const guint32 id);
+
+gboolean    gsignond_secret_storage_check_credentials (
+                  GSignondSecretStorage *self, const guint32 id,
+                  GString *username, GString *password);
+
+GHashTable* gsignond_secret_storage_load_data (
+                GSignondSecretStorage *self, const guint32 id,
+                const guint32 method);
+
+gboolean    gsignond_secret_storage_update_data (
+                GSignondSecretStorage *self, const guint32 id,
+                const guint32 method, const GHashTable *data);
+
+gboolean    gsignond_secret_storage_remove_data (
+                GSignondSecretStorage *self, const guint32 id,
+                const guint32 method);
+
+G_END_DECLS
+
+#endif /* __GSIGNOND_SECRET_STORAGE_H__ */
