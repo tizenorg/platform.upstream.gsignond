9384ac2bd8095dca5217efb6183e08fc481f60b1
Amarnath Valluri <amarnath.valluri@linux.intel.com>
2013-02-19 17:48:49 +0200

    daemon: Fix issues in unix signal handling.
    Usiging glib_unix_source for handing of unix signals.

--- src/daemon/main.c
+++ src/daemon/main.c
@@ -26,7 +26,8 @@
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
-#include <sys/socket.h>
+#include <stdio.h>
+#include <glib-unix.h>
 #include <glib.h>
 #include <gio/gio.h>
 
@@ -35,93 +36,60 @@
 #include "daemon/dbus/gsignond-dbus.h"
 
 static GSignondDaemon *_daemon;
-static GIOChannel     *_sig_channel;
-static int             _sig_fd[2];
+static guint           _sig_source_id[3];
 
-static gboolean 
-_handle_unix_signal (GIOChannel *channel,
-                     GIOCondition condition,
-                     gpointer data)
+static gboolean
+_handle_quit_signal (gpointer user_data)
 {
-    int signal = 0;
-    int ret = read (_sig_fd[1], &signal, sizeof(signal));
+    GMainLoop *ml = (GMainLoop *) user_data;
 
-    if (ret == -1) {
-        ERR ("failed to read signal value: %s", strerror(errno));
-        return FALSE;
-    }
+    g_return_val_if_fail (ml != NULL, FALSE);
+    DBG ("Received quit signal");
+    if (ml) g_main_loop_quit (ml);
 
-    switch (signal) {
-        case SIGHUP: {
-            DBG ("Received SIGHUP");
-            //TODO: restart daemon
-            break;
-        }
-        case SIGTERM: {
-            DBG ("Received SIGTERM");
-            //TODO: stop daemon
-            break;
-        }
-        case SIGINT:  {
-            DBG ("Received SIGINT");
-            //TODO: stop daemon
-            break;
-        }
-        default: break;
+    return FALSE;
+}
+
+static gboolean
+_handle_reload_signal (gpointer user_data)
+{
+    GSignondDaemon **daemon = (GSignondDaemon **) user_data;
+
+    DBG ("Received reload signal");
+    g_return_val_if_fail (daemon != NULL, FALSE);
+    if (*daemon) {
+        g_object_unref (*daemon);
+        DBG ("Restarting daemon ....");
+        *daemon = gsignond_daemon_new ();
     }
 
     return TRUE;
 }
 
-static void
-_setup_signal_handlers ()
-{
-    if (socketpair (AF_UNIX, SOCK_STREAM, 0, _sig_fd) != 0) {
-        ERR( "Couldn't create HUP socketpair");
-        return;
-    }
-
-    _sig_channel = g_io_channel_unix_new (_sig_fd[0]);
-    g_io_add_watch (_sig_channel, 
-                    G_IO_IN, 
-                    _handle_unix_signal,
-                    NULL);
-}
-
-static void
-_unset_signal_handlers ()
-{
-    if (_sig_channel) {
-        g_io_channel_unref (_sig_channel);
-        _sig_channel = 0;
-    }
-
-    close (_sig_fd[0]);
-    close (_sig_fd[1]);
-}
-
 static void 
-_signal_handler (int signal)
+_install_sighandlers (GMainLoop *main_loop)
 {
-    int ret = write (_sig_fd[0], &signal, sizeof(signal));
+    GSource *source = NULL;
+    GMainContext *ctx = g_main_loop_get_context (main_loop);
 
-    (void) ret;
-}
-
-static void 
-_install_sighandlers ()
-{
-    struct sigaction act;
-
-    _setup_signal_handlers();
-
-    act.sa_handler = _signal_handler;
-    sigemptyset (&act.sa_mask);
-    act.sa_flags = SA_RESTART;
-
-    sigaction (SIGHUP, &act, 0);
-    sigaction (SIGTERM, &act, 0);
-    sigaction (SIGINT, &act, 0);
+    source = g_unix_signal_source_new (SIGHUP);
+    g_source_set_callback (source,
+                           _handle_reload_signal,
+                           &_daemon,
+                           NULL);
+    _sig_source_id[0] = g_source_attach (source, ctx);
+    source = g_unix_signal_source_new (SIGTERM);
+    g_source_set_callback (source,
+                           _handle_quit_signal,
+                           main_loop,
+                           NULL);
+    _sig_source_id[1] = g_source_attach (source, ctx);
+    source = g_unix_signal_source_new (SIGINT);
+    g_source_set_callback (source,
+                           _handle_quit_signal,
+                           main_loop,
+                           NULL);
+    _sig_source_id[2] = g_source_attach (source, ctx);
 }
 
 static void
@@ -139,9 +107,9 @@ _on_name_lost (GDBusConnection *connection,
                const gchar     *name,
                gpointer         user_data)
 {
-    GMainLoop *loop = (GMainLoop *) user_data;
+    GMainLoop *ml = (GMainLoop *) user_data;
     INFO ("Lost (or failed to acquire) the name '%s' on the session message bus", name);
-    g_main_loop_quit (loop);
+    if (ml) g_main_loop_quit (ml);
 }
 
 static void
@@ -155,12 +123,12 @@ _on_name_acquired (GDBusConnection *connection,
 int main (int argc, char **argv)
 {
     GError *error = NULL;
+    GMainLoop *main_loop = NULL;
     GOptionContext *opt_context = NULL;
     guint name_owner_id = 0;
     GOptionEntry opt_entries[] = {
         {NULL }
     };
-    GMainLoop *loop = 0;
 
     g_type_init ();
 
@@ -172,9 +140,8 @@ int main (int argc, char **argv)
         return -1;
     }
 
-    _install_sighandlers();
-
-    loop = g_main_loop_new (NULL, FALSE);
+    main_loop = g_main_loop_new (NULL, FALSE);
+    _install_sighandlers(main_loop);
 
     name_owner_id = g_bus_own_name (G_BUS_TYPE_SESSION,
             GSIGNOND_SERVICE, // "com.google.code.AccountsSSO.SingleSignOn",
@@ -183,16 +150,16 @@ int main (int argc, char **argv)
             _on_bus_acquired,
             _on_name_acquired,
             _on_name_lost,
-            loop, NULL);
+            main_loop, NULL);
 
     INFO ("Entering main event loop");
 
-    g_main_loop_run (loop);
+    g_main_loop_run (main_loop);
 
-    _unset_signal_handlers ();
     g_bus_unown_name (name_owner_id);
 
-    if (_daemon) g_object_unref (_daemon);
+    if (_daemon) g_object_unref (G_OBJECT (_daemon));
+    if (main_loop) g_main_loop_unref (main_loop);
 
     return 0;
 }
