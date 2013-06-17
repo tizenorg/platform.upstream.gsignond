42229723e24b28370e88c71cedd2caeee48950c8
Imran Zaman <imran.zaman@linux.intel.com>
2013-06-17 17:10:33 +0300

    script to extract release tar ball and create packaging folder for tizen dist

---
+++ tools/prepare-tizen.sh
@@ -0,0 +1,22 @@
+# $1 corresponds to gsignond-<ver>.tar.gz 
+# $2 is the destination folder
+# NOTE: all the files will be extracted under destination folder (instead of destfolder/gsignond-<version>)
+
+if [ $# -ne 2 -o -z "$1" -o -z "$2" ]; then
+    echo "Invalid arguments supplied"
+    echo "Usage: ./prepare-tizen.sh gsignond-<version>.tar.gz destfolder"
+    echo "NOTE: All the files will be extracted under destfolder (instead of destfolder/gsignond-<version>)"
+    exit
+fi
+
+currdir = `pwd`;
+echo "CURR dir = $currdir"
+
+mkdir -p $2 && \
+tar -xzvf $1 -C $2 --strip-components 1 && \
+cd $2 && \
+mkdir -p packaging && \
+cd packaging && \
+ln -sf ../dist/rpm/gsignond-tizen.spec gsignond.spec &&
+ln -sf ../dist/rpm/gsignond-tizen.changes gsignond.change;
+
