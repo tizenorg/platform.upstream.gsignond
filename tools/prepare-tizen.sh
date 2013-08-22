efd430f1f84cc9b6c2b35e393413a4fcb4fbfb1d
Amarnath Valluri <amarnath.valluri@linux.intel.com>
2013-08-22 14:22:54 +0300

    tools: fixes in tizen packaging script

--- tools/prepare-tizen.sh
+++ tools/prepare-tizen.sh
@@ -9,13 +9,13 @@ if [ $# -ne 2 -o -z "$1" -o -z "$2" ]; then
     exit
 fi
 
-currdir = `pwd`;
+currdir=`pwd`;
 echo "CURR dir = $currdir"
 
 mkdir -p $2 && \
 cd $2 && \
 git rm -r * && \
-tar -xzvf $1 -C $2 --strip-components 1 && \
+tar -xzvf $currdir/$1 -C $2 --strip-components 1 && \
 mkdir -p packaging && \
 cd packaging && \
 cp -f ../dists/rpm/gsignond-tizen.spec gsignond.spec && \
